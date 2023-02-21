370a5b1bca576bb6e2cc5a86508770211eb5c70f
Admin <admin@Admins-MacBook-Pro.local>
2023-02-21 13:19:26 -0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+import { onMounted } from "vue";
+onMounted(() => {
+  console.log("home view has mounted");
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
